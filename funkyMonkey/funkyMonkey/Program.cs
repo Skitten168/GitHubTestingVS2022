9af8e7e04061a77b2799b66078fd916508e78b63
Skitten168 <89822682+Skitten168@users.noreply.github.com>
2022-09-04 10:08:43 -0400

    Add files via upload

---
+++ funkyMonkey/funkyMonkey/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+using System.Globalization;
+class SalesTaxDemo
+{
+    static void Main()
+    {
+        Sale[] eachSale = new Sale[10];
+    }
+}
+class Sale
+{
+    public string InventoryNumber { get; set; }
+    public double Amount { get; set; }
+    public readonly double Tax;
+private void CalculateTax()
+{
+    // Write your CalculateTax method here.
+}
+}
